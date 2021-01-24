9bfc82ce72b9b34445ac7788975bd1e19a1781ba
Alexander Leskov <dsl_92@mail.ru>
2021-01-24 20:29:29 +0300

    Fixed ticket status update

--- lib/snitcher/ticket.rb
+++ lib/snitcher/ticket.rb
@@ -13,12 +13,12 @@ module Zendesk
           zd_reply_count = zd_value_by(:reply_count, ticket["custom_fields"])
           s_thread_messages = slack_thread(zd_thread_ts, 100)["messages"]
           s_reply_count = s_thread_messages.first["reply_count"]
-          unless reply_count_equal?(zd_reply_count, s_reply_count) # || last_reply_by_bot?(s_thread_messages)
+          unless reply_count_equal?(zd_reply_count, s_reply_count)
             if ticket["status"] == "closed"
               send_message(Zendesk::Text.ticket_closed(ticket["id"]), zd_thread_ts)
             else
               update_comments(ticket["id"], s_thread_messages.drop(zd_reply_count.to_i))
-              update_status(ticket["id"], options[:to], s_reply_count)
+              update_status(ticket["id"], options[:to], s_reply_count) unless last_reply_by_bot?(s_thread_messages)
               updated_ids << ticket["id"]
             end
           end
