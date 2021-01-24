26958cabcf915fa903f87d60e124be8b666fafca
Alexander Leskov <dsl_92@mail.ru>
2021-01-24 18:53:04 +0300

    Updated deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "~> 3.15.0"
 
 set :application, "zendesk-helper"
 set :repo_url, "https://github.com/alexleskov/zendesk-helper.git"
