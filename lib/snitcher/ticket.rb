9ee3450df6929d14138741b9068292c63b46bbf0
Alexander Leskov <dsl_92@mail.ru>
2021-01-25 22:15:52 +0300

    After Rubocop

--- lib/snitcher/ticket.rb
+++ lib/snitcher/ticket.rb
@@ -13,19 +13,19 @@ module Zendesk
           zd_reply_count = zd_value_by(:reply_count, ticket["custom_fields"])
           s_thread_messages = slack_thread(zd_thread_ts, 100)["messages"]
           s_reply_count = s_thread_messages.first["reply_count"]
-          unless reply_count_equal?(zd_reply_count, s_reply_count)
-            case ticket["status"]
-            when "closed"
-              send_message(Zendesk::Text.ticket_closed(ticket["id"]), zd_thread_ts) if !last_reply_by_bot?(s_thread_messages)
+          next if reply_count_equal?(zd_reply_count, s_reply_count)
+
+          case ticket["status"]
+          when "closed"
+            send_message(Zendesk::Text.ticket_closed(ticket["id"]), zd_thread_ts) unless last_reply_by_bot?(s_thread_messages)
+          else
+            update_comments(ticket["id"], s_thread_messages.drop(zd_reply_count.to_i))
+            if last_reply_by_bot?(s_thread_messages)
+              update_reply_count(ticket["id"], s_reply_count)
             else
-              update_comments(ticket["id"], s_thread_messages.drop(zd_reply_count.to_i))
-              if last_reply_by_bot?(s_thread_messages)
-                update_reply_count(ticket["id"], s_reply_count)
-              else
-                update_status(ticket["id"], options[:to], s_reply_count)
-              end
-              updated_ids << ticket["id"]
+              update_status(ticket["id"], options[:to], s_reply_count)
             end
+            updated_ids << ticket["id"]
           end
         end
         updated_ids
