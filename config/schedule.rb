878208223a4133c43aedce925f0531077cc70eef
Alexander Leskov <dsl_92@mail.ru>
2021-01-26 10:23:56 +0300

    Changed scheduler period

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,7 +9,7 @@ require './application'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '2m', name: "Check tickets" do |job|
+scheduler.every '3m', name: "Check tickets" do |job|
   Zendesk::Snitcher::Pattern::Default.new.go
 end
 
