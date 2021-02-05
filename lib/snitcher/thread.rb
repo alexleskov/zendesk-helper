a91ca3d70d515f2017d6d0e4cc00b2af83a8c0ed
Alexander Leskov <dsl_92@mail.ru>
2021-02-05 19:58:20 +0300

    Minor fix for threads

--- lib/snitcher/thread.rb
+++ lib/snitcher/thread.rb
@@ -4,19 +4,17 @@ module Zendesk
   class Snitcher
     class Thread < Zendesk::Snitcher
       def do_action(ticket, thread, options)
-        actions_by(ticket, thread, options) unless thread.reaction_include?(ticket.reaction_by_status)
+        actions_by(ticket, thread) unless thread.reaction_include?(ticket.reaction_by_status)
       end
 
       private
 
-      def actions_by(ticket, thread, _options)
+      def actions_by(ticket, thread)
         on_update_params = { channel_id: channel_id, thread_ts: ticket.thread_ts.to_s }
         if thread.reactions_by_bot && !thread.reactions_by_bot.empty?
-          on_update_params[:name] = thread.reactions_by_bot.first["name"]
-          slack.reactions_remove(on_update_params).push
+          slack.reactions_remove(on_update_params.merge(name: thread.reactions_by_bot.first["name"])).push
         end
-        on_update_params[:name] = ticket.reaction_by_status
-        slack.reactions_add(on_update_params).push
+        slack.reactions_add(on_update_params.merge(name: ticket.reaction_by_status)).push
         on_update_params[:text] = Zendesk::Text.ticket_on_status(ticket.status, ticket.id)
         slack.chat_post_message(on_update_params).push if on_update_params[:text]
       end
