47507c4022b506bea8ba78fdbba6bfde600d547f
Alexander Leskov <dsl_92@mail.ru>
2021-02-06 18:23:38 +0300

    Updated ticket snitcher

--- lib/snitcher/ticket.rb
+++ lib/snitcher/ticket.rb
@@ -10,19 +10,17 @@ module Zendesk
       private
 
       def actions_by(ticket, thread, options)
+        return unless thread.last_reply_by_bot?
+
         case ticket.status
         when "closed"
           text = Zendesk::Text.ticket_closed(ticket.id)
-          slack.chat_post_message(text: text, channel_id: channel_id, thread_ts: ticket.thread_ts.to_s).push if text && !thread.last_reply_by_bot?
+          slack.chat_post_message(text: text, channel_id: channel_id, thread_ts: ticket.thread_ts.to_s).push if text
         else
-          unless thread.messages.empty?
-            zendesk.ticket(id: ticket.id, comment: thread.to_comments(drop: ticket.reply_count), public_mode: false).update
-          end
-          unless thread.last_reply_by_bot?
-            on_update_params = { id: ticket.id, custom_fields: [{ "id" => zd_reply_count_field_id, "value" => thread.reply_count }] }
-            on_update_params[:status] = options[:to]
-            zendesk.ticket(on_update_params).update
-          end
+          on_update_params = { id: ticket.id, custom_fields: [{ "id" => zd_reply_count_field_id, "value" => thread.reply_count }],
+                               public_mode: false, status: options[:to] }
+          on_update_params[:comment] = thread.to_comments(drop: ticket.reply_count) unless thread.messages.empty?
+          zendesk.ticket(on_update_params).update
         end
       end
     end
