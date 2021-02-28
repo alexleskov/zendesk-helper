3e01f9ecfdcb55e8ecba3e1b89b67764cbbc882e
Alexander Leskov <dsl_92@mail.ru>
2021-02-28 20:15:29 +0300

    Added logging for thread snitcher

--- lib/snitcher/thread.rb
+++ lib/snitcher/thread.rb
@@ -12,11 +12,13 @@ module Zendesk
       def actions_by(ticket, thread)
         on_update_params = { channel_id: channel_id, thread_ts: ticket.thread_ts.to_s }
         if thread.reactions_by_bot
-          slack.reactions_remove(on_update_params.merge(name: thread.reactions_by_bot.first["name"])).push
+          remove_result = slack.reactions_remove(on_update_params.merge(name: thread.reactions_by_bot.first["name"])).push
+          p "Remove reaction. - Ticket: #{ticket.id}, result: #{remove_result}"
         end
-        slack.reactions_add(on_update_params.merge(name: ticket.reaction_by_status)).push
+        add_result = slack.reactions_add(on_update_params.merge(name: ticket.reaction_by_status)).push
         on_update_params[:text] = Zendesk::Text.ticket_on_status(ticket.status, ticket.id)
         slack.chat_post_message(on_update_params).push if on_update_params[:text]
+        p "Thread reaction include ticket status reaction? - Ticket: #{ticket.id} - #{thread.reaction_include?(ticket.reaction_by_status)}, result: #{add_result}"
       end
     end
   end
