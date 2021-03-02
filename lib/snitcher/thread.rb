dde758b60ed460dd3d9e2e97371b5aa1a13cf5f5
Alexander Leskov <dsl_92@mail.ru>
2021-03-02 14:28:14 +0300

    Fix false message about status in thread

--- lib/snitcher/thread.rb
+++ lib/snitcher/thread.rb
@@ -4,6 +4,8 @@ module Zendesk
   class Snitcher
     class Thread < Zendesk::Snitcher
       def do_action(ticket, thread, _options)
+        raise "Can't find reaction name by ticket status" unless ticket.reaction_by_status
+
         actions_by(ticket, thread) unless thread.reaction_include?(ticket.reaction_by_status)
       end
 
@@ -13,12 +15,10 @@ module Zendesk
         on_update_params = { channel_id: channel_id, thread_ts: ticket.thread_ts.to_s }
         if thread.reactions_by_bot
           remove_result = slack.reactions_remove(on_update_params.merge(name: thread.reactions_by_bot.first["name"])).push
-          p "Remove reaction. - Ticket: #{ticket.id}, result: #{remove_result}"
         end
         add_result = slack.reactions_add(on_update_params.merge(name: ticket.reaction_by_status)).push
         on_update_params[:text] = Zendesk::Text.ticket_on_status(ticket.status, ticket.id)
-        slack.chat_post_message(on_update_params).push if on_update_params[:text]
-        p "Thread reaction include ticket status reaction? - Ticket: #{ticket.id} - #{thread.reaction_include?(ticket.reaction_by_status)}, result: #{add_result}"
+        slack.chat_post_message(on_update_params).push if on_update_params[:text] && add_result["ok"]
       end
     end
   end
